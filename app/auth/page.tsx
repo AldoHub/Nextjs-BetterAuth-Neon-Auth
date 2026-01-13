00e7799bb5b7f261a931037cc4326b0b941d779c
aldocaava <aldocaava@hotmail.com>
2026-01-13 15:45:06 -0500

    added basic auth and session handling

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,15 @@
 import AuthClientPage from "./auth-client";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function AuthPage() {
+ const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session) {
+   redirect("/dashboard");
+  }
+
+
   return <AuthClientPage />;
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,14 @@
 import DashboardClientPage from "./dashboard-client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
 
 export default async function DashboardPage() {
+
+   const session = await auth.api.getSession({ headers: await headers() });
+  
+    if (!session) {
+     redirect("/auth");
+    }
   return <DashboardClientPage />;
 }
